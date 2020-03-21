93cbeabfef118c926632d4242572ae70ab27063e
Laura Collins <laura.e.collins1@gmail.com>
2020-03-21 09:28:06 -0500

    Add project files.

---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Sarah" }, { "age", "38" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Chris" }, { "age", "40" } });
+            myFamily.Add("father", new Dictionary<string, string>() { { "name", "Clifford" }, { "age", "71" } });
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Kimberly" }, { "age", "59" } });
+
+
+        }
+    }
+}
